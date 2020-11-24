9beac6b86a20cab564cc4caa5ed3dce3bb994304
Krzysztof Nasuta <krzysiek.com.pl@wp.pl>
2020-11-24 21:25:26 +0100

    Added detail view

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
 from products.models import *
 
-admin.site.register(Category)
-admin.site.register(Subcategory)
+
+class SubcategoryInline(admin.TabularInline):
+    model = Subcategory
+    extra = 1
+
+
+class ProductInline(admin.TabularInline):
+    model = Product
+    extra = 1
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [SubcategoryInline]
+
+
+class SubcategoryAdmin(admin.ModelAdmin):
+    inlines = [ProductInline]
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Subcategory, SubcategoryAdmin)
 admin.site.register(Product)
-
-
--- products/models.py
+++ products/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 
+def category_list_fun(order="name"):
+    return Category.objects.all().order_by(order)
+
+
 class Category(models.Model):
     def __str__(self):
         return self.name
@@ -15,6 +19,9 @@ class Category(models.Model):
                 product_list.append(product)
         return product_list
 
+    def category_list(self, order="name"):
+        return Category.objects.all().order_by(order)
+
     name = models.CharField(max_length=256, default='')
     shortName = models.CharField(max_length=32, default='')
 
@@ -28,6 +35,7 @@ class Subcategory(models.Model):
 
     name = models.CharField(max_length=256)
     shortName = models.CharField(max_length=32, default='')
+    tags = models.CharField(max_length=1024, default='')
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 
@@ -44,3 +52,11 @@ class Product(models.Model):
     subcategory = models.ForeignKey(Subcategory, on_delete=models.CASCADE)
     brand = models.CharField(max_length=128, default='')
     tags = models.CharField(max_length=1024, default='')
+    about = models.CharField(max_length=4096, default='')
+    photo = models.CharField(max_length=256, default='')
+
+    def Subcategory(self):
+        return self.subcategory
+
+    def Category(self):
+        return self.Subcategory().category
