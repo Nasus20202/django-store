a62a7b10b9a17718a4fb2db9bd91ffa9a9d90782
Krzysztof Nasuta <krzysiek.com.pl@wp.pl>
2020-12-02 19:27:17 +0100

    Added search functionality

--- products/urls.py
+++ products/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = "products"
 urlpatterns = [
     path('', views.index_view, name='index'),
+    path('search/', views.search, name='search'),
     path('product/<str:product_name>/', views.product_view, name='product'),
     path('<str:category_name>/', views.category_view, name='category'),
     path('<str:category_name>/<str:subcategory_name>/', views.subcategory_view, name='subcategory'),
