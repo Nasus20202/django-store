673f0bc86c44ca30053b644775f3fa46e562a870
Krzysztof Nasuta <krzysiek.com.pl@wp.pl>
2020-12-09 18:25:36 +0100

    Update admin.py

--- products/admin.py
+++ products/admin.py
@@ -20,7 +20,18 @@ class SubcategoryAdmin(admin.ModelAdmin):
     inlines = [ProductInline]
 
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'brand', 'stock', 'price')
+    search_fields = ['name', 'brand', 'shortName']
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_filter = ['date']
+    search_fields = ['orderId', 'name', 'surname', 'email']
+    list_display = ('orderId', 'email' ,'cost')
+
+
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Subcategory, SubcategoryAdmin)
-admin.site.register(Product)
-admin.site.register(Order)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order, OrderAdmin)
