95f49f4bbe9a06ea326448d0183bb7b48b5285cf
Krzysztof Nasuta <47270121+Nasus20202@users.noreply.github.com>
2021-01-26 10:17:47 +0100

    Update settings.py

--- shop/settings.py
+++ shop/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(pwsw)%@k@(eyv5%5s93r#y)+^hs!jyt1g57#yd9^5gg9-z@y3'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
