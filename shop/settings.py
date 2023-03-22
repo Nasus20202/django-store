f2270f13856e93b8d1ee96f57d0a77bcb03c63f0
Krzysztof Nasuta <krzysztof.nasuta@wp.pl>
2023-03-22 17:34:50 +0100

    update to new version

--- shop/settings.py
+++ shop/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1'
+]
 
 
 # Application definition
